760a1c0b80f866180800577e8bd1960eca09db2f
Matheus Ramos <theussramos@gmail.com>
2023-01-24 21:32:39 -0300

    exercícios feitos até 09

---
+++ fundamentos/secao-03-java-script/dia-01-java-script-primeiros-passos/exercicios-pratica07.js
@@ -0,0 +1,9 @@
+const num1 = 10;
+const num2 = 15;
+const num3 = 23;
+
+if (num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0) {
+    console.log('Um dos números é par!')
+} else {
+    console.log('Não há um número par!')
+}
