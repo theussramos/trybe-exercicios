15462b72fafb2225c48f42ddcebb6cbc3ea87a09
Matheus Ramos <theussramos@gmail.com>
2023-01-26 16:51:25 -0300

    adicionado exercicios function

---
+++ fundamentos/secao-04-objetos-e-funcoes/exercicios-funcoes.js
@@ -0,0 +1,16 @@
+// function darBoasVindas (nome, idade) {
+//     return 'Boas vindas, ' + nome + ', ' + idade + '!';
+// }
+// let retornoFuncao = darBoasVindas ('Matheus', 27);
+// console.log(retornoFuncao);
+// console.log(darBoasVindas('Matheus', 27));
+
+
+
+
+function soma(numero1, numero2, numero3, numero4, numero5) {
+    // let resultado = numero1 + numero2 + numero3 + numero4 - numero5;
+    // return resultado;
+   return numero1 + numero2 + numero3 + numero4 - numero5;
+}
+console.log(soma(13, 19, 15, 12, 10));
