881f91a1d850fd83a3bcace8f528d913ac3ff8b5
Matheus Ramos <theussramos@gmail.com>
2023-02-16 15:45:26 -0300

    adicionado exemplos

---
+++ fundamentos/secao-06-js-es6-testes-unitarios/dia-01-fluxo-excecoes-manipulacao-objetos/exemplo-3-aula.js
@@ -0,0 +1,51 @@
+const customer = {
+    firstName: 'Roberto',
+    age: 22,
+    job: 'Teacher',
+  };
+  
+  let newKey = 'lastName';
+  const lastName = 'Ferreira';
+  
+  const adicionaPropriedade = (objeto, novaPropriedade, valor) => {
+    objeto[novaPropriedade] = valor;
+  };
+  
+  adicionaPropriedade(customer, newKey, lastName);
+  console.log(customer);
+
+
+
+
+
+
+// Desta forma, sempre que houver a necessidade de criar novas propriedades para o objeto, 
+// basta declarar as novas variáveis referentes às novas chaves e valores e chamar a função novamente,
+//  como no exemplo abaixo, em que foi criada a chave fullName:
+
+
+
+const customer = {
+    firstName: 'Roberto',
+    age: 22,
+    job: 'Teacher',
+  };
+  
+  let newKey = 'lastName';
+  const lastName = 'Ferreira';
+  
+  const adicionaPropriedade = (objeto, novaPropriedade, valor) => {
+    objeto[novaPropriedade] = valor
+  };
+  
+  adicionaPropriedade(customer, newKey, lastName);
+  console.log(customer);
+  
+
+
+  newKey = 'fullName';
+  const fullName = `${customer.firstName} ${customer.lastName}`;
+  
+
+  adicionaPropriedade(customer, newKey, fullName);
+  console.log(customer);
