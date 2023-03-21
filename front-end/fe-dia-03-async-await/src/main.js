8017f41c1f8b8030959b80a21ab099082c0810b7
Matheus Ramos <theussramos@gmail.com>
2023-03-21 20:45:50 -0300

    finalizado CEP

--- front-end/fe-dia-03-async-await/src/main.js
+++ front-end/fe-dia-03-async-await/src/main.js
@@ -1,2 +1,14 @@
 import './style.css';
+import getAddressFromCep from './getAddressFromCep';
+document.querySelector('button').addEventListener('click', handleClick);
 
+export async function handleClick() {
+    const cep = document.querySelector('input').value;
+
+    try {
+        const adressData = await getAddressFromCep(cep);
+        document.querySelector('pre').innerHTML = JSON.stringify(adressData);
+    } catch (error) {
+        alert(error.message);
+    }
+}
