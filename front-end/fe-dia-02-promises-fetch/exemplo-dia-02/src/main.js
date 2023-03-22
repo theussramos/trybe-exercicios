02872dfe28dbbf0dde5d84be87973ca61ef6de47
Matheus Ramos <theussramos@gmail.com>
2023-03-21 22:49:28 -0300

    progresso herois dia 2

--- front-end/fe-dia-02-promises-fetch/exemplo-dia-02/src/main.js
+++ front-end/fe-dia-02-promises-fetch/exemplo-dia-02/src/main.js
@@ -1,1 +1,31 @@
 import './style.css';
+import Swal from 'sweetalert2';
+
+const img = document.querySelector('#image');
+const name = document.querySelector('#name');
+const button = document.querySelector('#button');
+
+// const ACCESS_TOKEN = '5995677377195107';
+// const BASE_URL = `https://www.superheroapi.com/api/${ACCESS_TOKEN}`;
+const BASE_URL = 'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id';
+
+const MAX_HEROES = 1000;
+const randomId = () => Math.floor(Math.random() * MAX_HEROES);
+
+button.addEventListener('click', (event) => {
+    event.preventDefault();
+    const id = randomId();
+
+    fetch(`${BASE_URL}/${id}.json`)
+        .then((result) => result.json())
+        .then((data) => {
+            img.src = data.images.lg;
+            name.innerHTML = data.name;
+        })
+        .catch((error) => Swal.fire({
+            title: 'Hero not found',
+            text: error.message,
+            icon: 'error',
+            confirmButtonText: 'Cool',
+        }));
+});
