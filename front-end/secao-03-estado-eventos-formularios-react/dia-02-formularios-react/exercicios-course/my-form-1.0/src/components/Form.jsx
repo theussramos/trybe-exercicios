cbbd7082508dce8e31c5613392a64fdfd0609237
Matheus Ramos <theussramos@gmail.com>
2023-04-04 15:10:40 -0300

    progresso exercicios course

---
+++ front-end/secao-03-estado-eventos-formularios-react/dia-02-formularios-react/exercicios-course/my-form-1.0/src/components/Form.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      email: event.target.value,
+    });
+  }
+
+  render() {
+    const { email } = this.state;
+
+    return (
+      <div>
+        <h1>Formulário em React!</h1>
+        <form className="form">
+
+          <div>
+            <label htmlFor="name">
+              Nome:<input id='name' name='name' type="text"></input>
+            </label>
+          </div>
+          <div>
+            <label htmlFor="email">
+              Email:<input id='email' name='email' type="email" onChange={this.handleChange} value={email}></input>
+              <label htmlFor="age">
+                Idade:
+                <select
+                  id="age"
+                  name="age"
+                  defaultValue="">
+                  <option value="">Selecione</option>
+                  <option value="adult">Maior que 18</option>
+                  <option value="underage">Menor que 18</option>
+                </select>
+              </label>
+
+              <label htmlFor='anecdote'>
+                Anedota:
+                <textarea id='anecdote' name='anecdote'></textarea>
+              </label>
+            </label>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Form;
