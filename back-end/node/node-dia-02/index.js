f3f43d60f868456a82ce44dd25ba00504e933075
Matheus Ramos <theussramos@gmail.com>
2023-07-14 14:54:49 -0300

    dia-01-node

--- back-end/node/node-dia-02/index.js
+++ back-end/node/node-dia-02/index.js
@@ -1,4 +1,4 @@
 const { brl, usdToBrl } = require('./brlValue');
 
 console.log(`Valor do dólar: ${brl}`); // Valor do dólar: 5.37
-console.log(`10 dólares em reais: ${usdToBrl(10)}`); // 10 dólares em reais: 53.7
+console.log(`10 dólares em reais: ${usdToBrl(10)}`); // 10 dólares em reais: 53.7git stat
